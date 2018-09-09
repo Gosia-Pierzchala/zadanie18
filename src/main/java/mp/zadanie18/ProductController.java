247b57f5b7e391230ac138b4a7fb3bfbf1e90bd1
Gosia-Pierzchala <gk.kalinowska@gmail.com>
2018-09-09 12:47:27 +0200

    poprawki

--- src/main/java/mp/zadanie18/ProductController.java
+++ src/main/java/mp/zadanie18/ProductController.java
@@ -3,6 +3,7 @@ package mp.zadanie18;
 import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -17,16 +18,38 @@ public class ProductController {
 
     @GetMapping("/lista")
     @ResponseBody
-    public String productsAndPrices(){
+    public String productsAndPrices(@RequestParam(value = "kategoria", required = false, defaultValue = "wszystkie") String category){
         List<Product> lista = productRepositorium.getProducts();
         String result = "";
         double sum = 0;
         for (Product product: lista) {
+            if(category.equals("wszystkie")){
             result = result + product.toString() + "<br>";
             double cena = product.getCena();
             sum = sum + cena;
-        }
-        return result + "<br>" + "Suma cen wyżej pokazanych produktów to: " + sum;
+        } else if(category.equals("spozywcze")){
+                String kategoria = product.getKategoria();
+                if(kategoria.equals("artykuły spożywcze")){
+                    result = result + product.toString() + "<br>";
+                    double cena = product.getCena();
+                    sum = sum + cena;
+                }
+            } else if(category.equals("domowe")){
+                String kategoria = product.getKategoria();
+                if(kategoria.equals("artykuły gosp. domowego")){
+                    result = result + product.toString() + "<br>";
+                    double cena = product.getCena();
+                    sum = sum + cena;
+                }
+            } else if(category.equals("inne")){
+                String kategoria = product.getKategoria();
+                if(kategoria.equals("inne")){
+                    result = result + product.toString() + "<br>";
+                    double cena = product.getCena();
+                    sum = sum + cena;
+                }
+            }
     }
-
+    return result + "<br>" + "Suma cen wyżej pokazanych produktów to: " + sum;
+    }
 }
